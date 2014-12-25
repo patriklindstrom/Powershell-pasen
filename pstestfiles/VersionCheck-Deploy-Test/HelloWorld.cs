f37ada12c6a9d148da56984ac85db12f36821ed6
Patrik Lindstrom <patriklindstrom@hotmail.com>
2014-12-25 12:37:26 +0100

    TestFiles
    Added two testfiles. Do not include the Getting Started with Asp net
    test project.

---
+++ pstestfiles/VersionCheck-Deploy-Test/HelloWorld.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Hello
+{
+   public static void Main(string[] args)
+   {
+      Console.WriteLine("Hello, World!");
+      Console.WriteLine("You entered the following {0} command line arguments:",
+         args.Length );
+      for (int i=0; i < args.Length; i++)
+      {
+         Console.WriteLine("{0}", args[i]); 
+      }
+   }
+}
